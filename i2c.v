1f2350c243cdda4680058707322dceaad4e50906
Kieran Murray <s3660898@student.rmit.edu.au>
2020-06-09 16:09:23 +1000

    I2C module functional on its own

--- i2c.v
+++ i2c.v
@@ -7,12 +7,13 @@ module i2c(
   /* command related */
   input [6:0] cmd_address,
 
-  /* data output (sending) related */
-  output [7:0] data_out,
+  /* data for sending related */
+  input [7:0] data_0,
+  input [7:0] data_1,
 
   input start,
 
-  /* i2c io related */
+  /* i2c phy io related */
   tri1 scl,
   tri1 sda,
 
@@ -39,17 +40,20 @@ reg r_scl_pull;  /*pulls scl down to 0*/
 reg r_active;
 
 localparam [2:0]
-  STATE_START   = 3'd0,
-  STATE_ADDRESS = 3'd1,
-  STATE_CONT    = 3'd2,
-  STATE_DATA    = 3'd3,
-  STATE_END     = 3'd4;
+  STATE_START   = 3'd0, /*for the initial sda/scl pulling down*/
+  STATE_ADDRESS = 3'd1, /*for sending the address, write*/
+  STATE_DATA_0  = 3'd2, /*for sending the first data packet*/
+  STATE_DATA_1  = 3'd3, /*for sending the second data packet*/
+  STATE_END     = 3'd4; /*for the final sda/scl transition up*?
 
 /*for keeping track of the current state*/
-reg [1:0] r_state;
+reg [2:0] r_state;
 
 /*for counting the transitions in the 'start' state*/
 reg [1:0] r_state_start_count;
+
+/*for countig the transitions in the 'end' state*/
+reg [2:0] r_state_end_count;
 
 /*       
 *   BIT_STATE: the state within the current bit,
@@ -66,13 +70,16 @@ reg [1:0] r_state_start_count;
 *                0    1    2    0    1    etc
 */
 
-reg [1:0] r_bit_state;
-reg [3:0] r_bit_count;
-
 localparam [1:0]
   BIT_STATE_0 = 2'b00,
   BIT_STATE_1 = 2'b01,
   BIT_STATE_2 = 2'b10;
+
+reg [1:0] r_bit_state;
+reg [3:0] r_bit_count;
+
+reg [8:0] r_address_qualified;
+reg [8:0] r_data_qualified;
 
 always @(posedge(clk))
 begin
@@ -108,25 +115,32 @@ begin
           r_state = STATE_ADDRESS;
           r_bit_count = 0;
           r_bit_state = 0;
+          r_address_qualified[8:2] = cmd_address[6:0]; /*address*/
+          r_address_qualified[1]   = 0;                /*write*/
+          r_address_qualified[0]   = 1;                /*ack*/
         end
 
-        /*transition behaviour*/
-        case(r_state_start_count)
-          0:
-          begin
-            r_sda_pull = 1;
-            r_scl_pull = 0;
-          end
+        /* if not transitioning to address sending state*/
+        else
+        begin
+          /*i2c start sda/scl transition behaviour*/
+          case(r_state_start_count)
+            0:
+            begin
+              r_sda_pull = 1;
+              r_scl_pull = 0;
+            end
 
-          1:
-          begin
-            r_sda_pull = 1;
-            r_scl_pull = 1;
-          end
-        endcase
+            1:
+            begin
+              r_sda_pull = 1;
+              r_scl_pull = 1;
+            end
+          endcase
 
-        /*incrementation*/
-        r_state_start_count = r_state_start_count + 1;
+          /*incrementation*/
+          r_state_start_count = r_state_start_count + 1;
+        end
       end
 
       STATE_ADDRESS:
@@ -139,20 +153,21 @@ begin
           r_bit_count = r_bit_count + 1;
         end
 
-        /*aknowledgement behaviour*/
-        if(r_bit_count == 7)
+        /*next state exit condition, transition to data state*/
+        if(r_bit_count == 9)
         begin
-          r_sda_pull = 0;
+          r_state = STATE_DATA_0;
+          r_bit_count = 0;
+          r_data_qualified[8:1] = data_0[7:0]; /*data to be sent next*/
+          r_data_qualified[0]   = 1;           /*pull up for ack*/
+        end
 
-          /*exit condition to next state*/
-          if(r_bit_state == 3)
-          begin
-            r_state = STATE_DATA;
-            r_bit_state = BIT_STATE_0;
-            r_bit_count = 0;
-          end
+        /*if not transitioning to data sending state*/
+        else
+        begin
 
-          /*clocking on the aknowledgement signal*/
+          /*BIT STATE transition switching*/
+          r_sda_pull = !r_address_qualified[8-r_bit_count];
           case(r_bit_state)
             BIT_STATE_0:  r_scl_pull = 1;
             BIT_STATE_1:  r_scl_pull = 0;
@@ -161,40 +176,119 @@ begin
 
           r_bit_state = r_bit_state + 1;
         end
+      end
 
-        /*for bits 0-6*/
+      STATE_DATA_0:
+      begin
+        /*next bit condition*/
+        if(r_bit_state == 3)
+        begin
+          r_bit_state = BIT_STATE_0;
+          r_bit_count = r_bit_count + 1;
+        end
+
+        /*next state exit condition, transition to data state*/
+        if(r_bit_count == 9)
+        begin
+          r_state = STATE_DATA_1;
+          r_bit_count = 0;
+          r_data_qualified[8:1] = data_1[7:0]; /*data to be sent next*/
+          r_data_qualified[0]   = 1;           /*pull up for ack*/
+        end
+
+        /*if not transitioning to data sending state*/
         else
         begin
 
           /*BIT STATE transition switching*/
+          r_sda_pull = !r_data_qualified[8-r_bit_count];
           case(r_bit_state)
-            BIT_STATE_0:
-            begin
-              r_sda_pull = !cmd_address[6-r_bit_count];
-              r_scl_pull = 1;
-            end
-
-            BIT_STATE_1:
-            begin
-              r_sda_pull = !cmd_address[6-r_bit_count];
-              r_scl_pull = 0;
-            end
-
-            BIT_STATE_2:
-            begin
-              r_sda_pull = !cmd_address[6-r_bit_count];
-              r_scl_pull = 1;
-            end
+            BIT_STATE_0:  r_scl_pull = 1;
+            BIT_STATE_1:  r_scl_pull = 0;
+            BIT_STATE_2:  r_scl_pull = 1;
           endcase
 
           r_bit_state = r_bit_state + 1;
         end
-      endcase
-    end
+      end
+
+      STATE_DATA_1:
+      begin
+        /*next bit condition*/
+        if(r_bit_state == 3)
+        begin
+          r_bit_state = BIT_STATE_0;
+          r_bit_count = r_bit_count + 1;
+        end
+
+        /*next state exit condition, transition to data state*/
+        if(r_bit_count == 9)
+        begin
+          r_state = STATE_END;
+          r_bit_count = 0;
+          r_state_end_count = 0;
+          r_data_qualified[8:1] = data_1[7:0]; /*data to be sent next*/
+          r_data_qualified[0]   = 1;           /*pull up for ack*/
+        end
+
+        /*if not transitioning to data sending state*/
+        else
+        begin
+
+          /*BIT STATE transition switching*/
+          r_sda_pull = !r_data_qualified[8-r_bit_count];
+          case(r_bit_state)
+            BIT_STATE_0:  r_scl_pull = 1;
+            BIT_STATE_1:  r_scl_pull = 0;
+            BIT_STATE_2:  r_scl_pull = 1;
+          endcase
+
+          r_bit_state = r_bit_state + 1;
+        end
+      end
+
+      STATE_END:
+      begin
+
+        /*final exit transition*/
+        if(r_state_end_count == 3)
+        begin
+          r_active = 0;
+        end
+        else
+        begin
+
+          case(r_state_end_count)
+            0:
+            begin
+              r_sda_pull = 1;
+              r_scl_pull = 1;
+            end
+
+            1:
+            begin
+              r_sda_pull = 1;
+              r_scl_pull = 0;
+            end
+
+            2:
+            begin
+              r_sda_pull = 0;
+              r_scl_pull = 0;
+            end
+          endcase
+
+          r_state_end_count = r_state_end_count + 1;
+
+        end
+      end
+
+    endcase
   end
 end
 
 assign sda = r_sda_pull ? 1'b0 : 1'bZ;
 assign scl = r_scl_pull ? 1'b0 : 1'bZ;
+assign busy = r_active;
 
 endmodule
--- sim/i2c.tcl
+++ sim/i2c.tcl
@@ -6,8 +6,11 @@ proc i2c {} {
   add wave /i2c/rst
   add wave /i2c/sda
   add wave /i2c/scl
+  add wave /i2c/busy
 
-  add wave *
+  add wave /i2c/r_state
+
+  #add wave *
 
   # clock setup
   force -deposit clk 1 0, 0 {5ps} -repeat 10
@@ -18,10 +21,12 @@ proc i2c {} {
   force rst 0
   run 10
 
-  force cmd_address 7'b_101_0101
-  force data_out 8'b_1010_1010
+  #force cmd_address 7'b_101_0101
+  force cmd_address 7'b_101_1001
+  force data_0 8'b_1101_1011
+  force data_1 8'b_0010_0100
   force start 1
   run 10
   force start 0
-  run 200
+  run 1100
 }
