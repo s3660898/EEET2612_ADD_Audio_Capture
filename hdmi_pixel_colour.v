192b817fc6cbf9a23eb111ec76b6c1d01c1ffdf5
Kieran Murray <s3660898@student.rmit.edu.au>
2020-07-09 15:34:00 +1000

    Added numbers for channel selection

--- hdmi_pixel_colour.v
+++ hdmi_pixel_colour.v
@@ -21,6 +21,160 @@ reg [7:0] r_r;
 reg [7:0] r_g;
 reg [7:0] r_b;
 
+function text_is_white;
+  input [11:0] px_y;
+  input [11:0] px_x;
+  input [1:0] num;
+
+  reg is_white = 0;
+
+  `define SCALE_FACTOR 2
+  if((px_x>>`SCALE_FACTOR) < 8 && (px_y>>`SCALE_FACTOR) < 12)
+  begin
+    reg [11:0] px_x_text;
+    reg [11:0] px_y_text;
+    px_x_text = px_x>>`SCALE_FACTOR;
+    px_y_text = px_y>>`SCALE_FACTOR;
+
+    case(num)
+      2'd0:
+      /*channel number 1*/
+      case({px_y_text[3:0], px_x_text[2:0]})
+        {4'd1,3'd4}: is_white = 1'b1;
+        {4'd2,3'd3}: is_white = 1'b1;
+        {4'd2,3'd4}: is_white = 1'b1;
+        {4'd3,3'd1}: is_white = 1'b1;
+        {4'd3,3'd2}: is_white = 1'b1;
+        {4'd3,3'd3}: is_white = 1'b1;
+        {4'd3,3'd4}: is_white = 1'b1;
+        {4'd4,3'd3}: is_white = 1'b1;
+        {4'd4,3'd4}: is_white = 1'b1;
+        {4'd5,3'd3}: is_white = 1'b1;
+        {4'd5,3'd4}: is_white = 1'b1;
+        {4'd6,3'd3}: is_white = 1'b1;
+        {4'd6,3'd4}: is_white = 1'b1;
+        {4'd7,3'd3}: is_white = 1'b1;
+        {4'd7,3'd4}: is_white = 1'b1;
+        {4'd8,3'd3}: is_white = 1'b1;
+        {4'd8,3'd4}: is_white = 1'b1;
+        {4'd9,3'd1}: is_white = 1'b1;
+        {4'd9,3'd2}: is_white = 1'b1;
+        {4'd9,3'd3}: is_white = 1'b1;
+        {4'd9,3'd4}: is_white = 1'b1;
+        {4'd9,3'd5}: is_white = 1'b1;
+        {4'd9,3'd6}: is_white = 1'b1;
+      endcase
+
+      /*channel number 2*/
+      2'd1:
+      case({px_y_text[3:0], px_x_text[2:0]})
+        {4'd1,3'd2}: is_white = 1'b1;
+        {4'd1,3'd3}: is_white = 1'b1;
+        {4'd1,3'd4}: is_white = 1'b1;
+        {4'd1,3'd5}: is_white = 1'b1;
+        {4'd2,3'd1}: is_white = 1'b1;
+        {4'd2,3'd2}: is_white = 1'b1;
+        {4'd2,3'd5}: is_white = 1'b1;
+        {4'd2,3'd6}: is_white = 1'b1;
+        {4'd3,3'd1}: is_white = 1'b1;
+        {4'd3,3'd2}: is_white = 1'b1;
+        {4'd3,3'd5}: is_white = 1'b1;
+        {4'd3,3'd6}: is_white = 1'b1;
+        {4'd4,3'd5}: is_white = 1'b1;
+        {4'd4,3'd6}: is_white = 1'b1;
+        {4'd5,3'd4}: is_white = 1'b1;
+        {4'd5,3'd5}: is_white = 1'b1;
+        {4'd6,3'd3}: is_white = 1'b1;
+        {4'd6,3'd4}: is_white = 1'b1;
+        {4'd7,3'd2}: is_white = 1'b1;
+        {4'd7,3'd3}: is_white = 1'b1;
+        {4'd8,3'd1}: is_white = 1'b1;
+        {4'd8,3'd2}: is_white = 1'b1;
+        {4'd8,3'd5}: is_white = 1'b1;
+        {4'd8,3'd6}: is_white = 1'b1;
+        {4'd9,3'd1}: is_white = 1'b1;
+        {4'd9,3'd2}: is_white = 1'b1;
+        {4'd9,3'd3}: is_white = 1'b1;
+        {4'd9,3'd4}: is_white = 1'b1;
+        {4'd9,3'd5}: is_white = 1'b1;
+        {4'd9,3'd6}: is_white = 1'b1;
+      endcase
+
+      /*channel number 3*/
+      2'd2:
+      case({px_y_text[3:0], px_x_text[2:0]})
+        {4'd1,3'd2}: is_white = 1'b1;
+        {4'd1,3'd3}: is_white = 1'b1;
+        {4'd1,3'd4}: is_white = 1'b1;
+        {4'd1,3'd5}: is_white = 1'b1;
+        {4'd2,3'd1}: is_white = 1'b1;
+        {4'd2,3'd2}: is_white = 1'b1;
+        {4'd2,3'd5}: is_white = 1'b1;
+        {4'd2,3'd6}: is_white = 1'b1;
+        {4'd3,3'd5}: is_white = 1'b1;
+        {4'd3,3'd6}: is_white = 1'b1;
+        {4'd4,3'd5}: is_white = 1'b1;
+        {4'd4,3'd6}: is_white = 1'b1;
+        {4'd5,3'd3}: is_white = 1'b1;
+        {4'd5,3'd4}: is_white = 1'b1;
+        {4'd5,3'd5}: is_white = 1'b1;
+        {4'd6,3'd5}: is_white = 1'b1;
+        {4'd6,3'd6}: is_white = 1'b1;
+        {4'd7,3'd5}: is_white = 1'b1;
+        {4'd7,3'd6}: is_white = 1'b1;
+        {4'd8,3'd1}: is_white = 1'b1;
+        {4'd8,3'd2}: is_white = 1'b1;
+        {4'd8,3'd5}: is_white = 1'b1;
+        {4'd8,3'd6}: is_white = 1'b1;
+        {4'd9,3'd2}: is_white = 1'b1;
+        {4'd9,3'd3}: is_white = 1'b1;
+        {4'd9,3'd4}: is_white = 1'b1;
+        {4'd9,3'd5}: is_white = 1'b1;
+      endcase
+
+      /*channel number 3*/
+      2'd3:
+      case({px_y_text[3:0], px_x_text[2:0]})
+        {4'd1,3'd5}: is_white = 1'b1;
+        {4'd1,3'd6}: is_white = 1'b1;
+        {4'd2,3'd4}: is_white = 1'b1;
+        {4'd2,3'd5}: is_white = 1'b1;
+        {4'd2,3'd6}: is_white = 1'b1;
+        {4'd3,3'd3}: is_white = 1'b1;
+        {4'd3,3'd4}: is_white = 1'b1;
+        {4'd3,3'd5}: is_white = 1'b1;
+        {4'd3,3'd6}: is_white = 1'b1;
+        {4'd4,3'd2}: is_white = 1'b1;
+        {4'd4,3'd3}: is_white = 1'b1;
+        {4'd4,3'd5}: is_white = 1'b1;
+        {4'd4,3'd6}: is_white = 1'b1;
+        {4'd5,3'd1}: is_white = 1'b1;
+        {4'd5,3'd2}: is_white = 1'b1;
+        {4'd5,3'd5}: is_white = 1'b1;
+        {4'd5,3'd6}: is_white = 1'b1;
+        {4'd6,3'd1}: is_white = 1'b1;
+        {4'd6,3'd2}: is_white = 1'b1;
+        {4'd6,3'd3}: is_white = 1'b1;
+        {4'd6,3'd4}: is_white = 1'b1;
+        {4'd6,3'd5}: is_white = 1'b1;
+        {4'd6,3'd6}: is_white = 1'b1;
+        {4'd6,3'd7}: is_white = 1'b1;
+        {4'd7,3'd5}: is_white = 1'b1;
+        {4'd7,3'd6}: is_white = 1'b1;
+        {4'd8,3'd5}: is_white = 1'b1;
+        {4'd8,3'd6}: is_white = 1'b1;
+        {4'd9,3'd4}: is_white = 1'b1;
+        {4'd9,3'd5}: is_white = 1'b1;
+        {4'd9,3'd6}: is_white = 1'b1;
+        {4'd9,3'd7}: is_white = 1'b1;
+      endcase
+    endcase
+  end
+
+  text_is_white = is_white;
+
+endfunction
+
 always @(posedge(clk))
 begin
   if(rst)
@@ -36,48 +190,9 @@ begin
       /*channel number in top left*/
       reg is_white = 1'b0;
       `define SCALE_FACTOR 2
-      if((px_x>>`SCALE_FACTOR) < 8 && (px_y>>`SCALE_FACTOR) < 12)
-      begin
-        /*
-        case({(px_y[3:0]>>`SCALE_FACTOR),(px_x[2:0]>>`SCALE_FACTOR)})
-        */
-        reg [11:0] px_x_text;
-        reg [11:0] px_y_text;
-        px_x_text = px_x>>`SCALE_FACTOR;
-        px_y_text = px_y>>`SCALE_FACTOR;
-        case({px_y_text[3:0], px_x_text[2:0]})
-          {4'd1,3'd4}: is_white = 1'b1;
-          {4'd2,3'd3}: is_white = 1'b1;
-          {4'd2,3'd4}: is_white = 1'b1;
-          {4'd3,3'd1}: is_white = 1'b1;
-          {4'd3,3'd2}: is_white = 1'b1;
-          {4'd3,3'd3}: is_white = 1'b1;
-          {4'd3,3'd4}: is_white = 1'b1;
-
-          {4'd4,3'd3}: is_white = 1'b1;
-          {4'd4,3'd4}: is_white = 1'b1;
-          {4'd5,3'd3}: is_white = 1'b1;
-          {4'd5,3'd4}: is_white = 1'b1;
-          {4'd6,3'd3}: is_white = 1'b1;
-          {4'd6,3'd4}: is_white = 1'b1;
-          {4'd7,3'd3}: is_white = 1'b1;
-          {4'd7,3'd4}: is_white = 1'b1;
-          {4'd8,3'd3}: is_white = 1'b1;
-          {4'd8,3'd4}: is_white = 1'b1;
-
-          {4'd9,3'd1}: is_white = 1'b1;
-          {4'd9,3'd2}: is_white = 1'b1;
-          {4'd9,3'd3}: is_white = 1'b1;
-          {4'd9,3'd4}: is_white = 1'b1;
-          {4'd9,3'd5}: is_white = 1'b1;
-          {4'd9,3'd6}: is_white = 1'b1;
-
-          default: is_white = 1'b0;
-        endcase
-      end
 
       /*rest of screen (giving channel number preference*/
-      if(is_white)
+      if(text_is_white(px_y, px_x, channel_select))
       begin
         r_r = 8'd255;
         r_g = 8'd255;
